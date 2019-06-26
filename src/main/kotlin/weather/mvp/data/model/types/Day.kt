c52025e72d2d0c0deb78bc16b5a17852ca387f62
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-06-26 13:52:41 +0700

    Add Day types

---
+++ src/main/kotlin/weather/mvp/data/model/types/Day.kt
@@ -0,0 +1,11 @@
+package main.kotlin.weather.mvp.data.model.types
+
+enum class Day(val day: String){
+    MON("Monday"),
+    TUE("Tuesday"),
+    WED("Wednesday"),
+    THU("Thursday"),
+    FRI("Friday"),
+    SAT("Saturday"),
+    SUN("Sunday");
+}

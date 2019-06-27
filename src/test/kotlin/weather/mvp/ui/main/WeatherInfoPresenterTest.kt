18014bc8b33e8a48c3e3b9d37832423f4cb55576
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-06-27 14:55:02 +0700

    Add test for WeatherInfoPresenter processInput

--- src/test/kotlin/weather/mvp/ui/main/WeatherInfoPresenterTest.kt
+++ src/test/kotlin/weather/mvp/ui/main/WeatherInfoPresenterTest.kt
@@ -2,14 +2,20 @@ package weather.mvp.ui.main
 
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
+import org.mockito.*
 import weather.mvp.data.WeatherInfoListRepository
+import weather.mvp.data.model.WeatherInfo
+import weather.mvp.data.specs.filter.FilterSpecification
+import weather.mvp.data.specs.filter.weatherInfo.AndFilter
+import weather.mvp.data.specs.filter.weatherInfo.CityFilter
+import weather.mvp.data.specs.filter.weatherInfo.StartDayFilter
 
 class WeatherInfoPresenterTest {
 
     private val items = weatherList
+
+    @Mock
+    private lateinit var mockItems: List<WeatherInfo>
 
     @Mock
     private lateinit var view: WeatherInfoPresenter.View
@@ -17,19 +23,30 @@ class WeatherInfoPresenterTest {
     @Mock
     private lateinit var repo: WeatherInfoListRepository
 
-    private lateinit var presenter: WeatherInfoPresenter
+    private lateinit var filter: FilterSpecification<WeatherInfo>
 
+    private lateinit var presenter: WeatherInfoPresenter
 
     @Before
     fun setup(){
         MockitoAnnotations.initMocks(this)
-        presenter = WeatherInfoPresenter(view, repo)
+        filter = AndFilter(CityFilter(), StartDayFilter())
+        presenter = WeatherInfoPresenter(view, repo, filter)
     }
 
     @Test
     fun `given presenter when presenter onCreate then repo calls add predefined weather items`() {
         presenter.onCreate()
         Mockito.verify(repo).add(items)
+    }
 
+    @Test
+    fun `given presenter when presenter processInput then repo queries result and view prints result`() {
+        val city = "Jakarta"
+        val day = "Monday"
+        val results = repo.query(filter, city, day)
+
+        presenter.processInput(city, day)
+        Mockito.verify(view).printResult(results)
     }
 }
